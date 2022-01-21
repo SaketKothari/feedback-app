574105399e0412f8532294b9a447bc753d4473ee
Saket Kothari <saketkothari722@gmail.com>
2022-01-21 14:31:22 +0530

    FeedbackList component & global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData); // app level state
+
   return (
     <>
       <Header />
       <div className="container">
-        <h1>App</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
