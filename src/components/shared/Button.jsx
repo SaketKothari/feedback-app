b002bfe3c7b44c8f31814cd5d16cbb9a0b6eabff
Saket Kothari <saketkothari722@gmail.com>
2022-01-22 10:36:49 +0530

    FeedbackForm input and Button component

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+// we want children because we're gonna be wrapping the component around for whatever   the text for button is and version is either primary or secondary
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  type: 'button',
+  isDisabled: false,
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
+
+export default Button;
